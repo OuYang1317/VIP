33a2cf28f676ed40d16f3fb6eaac5f18bc5b1d3e
Sakamoto <oushaoyang@yeah.net>
2019-06-21 19:49:59 +0800

    css

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('watch',function(){                       //监控文件状态
 })
 
 gulp.task('copyCss',function(){                    //转换scss文件
-    gulp.src('index.scss')
+    gulp.src('*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write())
@@ -26,12 +26,12 @@ gulp.task('copyCss',function(){                    //转换scss文件
 })
 
 gulp.task('zipCss',function(){                    //压缩CSS代码
-    gulp.src('dist/css/index.css').pipe(cleanCss())
+    gulp.src('dist/css/*.css').pipe(cleanCss())
     .pipe(rename({suffix:'-min'}))
     .pipe(gulp.dest('dist/css'))
 })
 gulp.task('Hejs',function(){                          //合并js  压缩js代码
-    gulp.src(["index.js",'a.js'])
+    gulp.src(["a.js",'b.js'])
     .pipe(concat('mian.js'))
     .pipe(gulp.dest('dist/js'))
     .pipe(uglify())
